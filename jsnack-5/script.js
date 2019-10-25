8c0935af5075b476139964cab66e499358f0defd
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-25 17:24:28 +0200

    clean code

--- jsnack-5/script.js
+++ jsnack-5/script.js
@@ -1,14 +1,14 @@
 
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 // un numero, se è dispari inseriscilo nell’array.
+//@ts-check
 
 var array = [];
-for(i=0; i < 6 ; i++){
-  var number = parseInt(prompt("inserisci un numero"))
+for(var i=0; i < 6 ; i++){
+  var number = parseInt(prompt("inserisci un numero"));
   if(number % 2 !== 0){
-    array.push(number)
+    array.push(number);
   }
-
 }
-document.getElementById("odd").innerHTML = "I numeri dispari da te inseriti sono :" + array
-console.log(array)
+document.getElementById("odd").innerHTML = "I numeri dispari da te inseriti sono :" + array;
+console.log(array);
