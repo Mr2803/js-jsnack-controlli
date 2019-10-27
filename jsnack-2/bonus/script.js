eb906e399ef07e280c86818c039965cd1f54a5f3
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-27 10:32:56 +0100

    add comment in jsnack2

--- jsnack-2/bonus/script.js
+++ jsnack-2/bonus/script.js
@@ -1,17 +1,23 @@
 //L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 
+
+//imposto le mie variabili
 var parola1 = "";
 var parola2 = "";
 
 var output1 = document.getElementById('parolaFirst');
 var output2 = document.getElementById('parolaSecond');
 
+
+//ciclo while per forzare l'utente ad inserire in parola1 una parola e non un numero/spazio vuoto
 while(!isNaN(parola1)){
   parola1 = prompt("inserisci una parola");
   if(!isNaN(parola1)){
     alert("Ti ho detto PAROLA")
   }
 }
+
+//ciclo while per forzare l'utente ad inserire in parola2 una parola e non un numero/spazio vuoto
 while(!isNaN(parola2)){
   parola2 = prompt("inserisci una parola");
   if(!isNaN(parola2)){
@@ -19,6 +25,7 @@ while(!isNaN(parola2)){
   }
 }
 
+//condizioni per stampare in pagina prima parola più breve
 if (parola1.length > parola2.length){
   output1.innerHTML = "la parola più corta è: " + parola2;
   output2.innerHTML = "la parola più lunga è: " + parola1;
