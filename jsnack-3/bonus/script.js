17e3a51195f4a710a4aa9d75cb8dbccb60f0bb90
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-27 10:45:17 +0100

    force user to insert a number in jsnack3 ( if it introduces a value different from a number, my index goes back by one)

--- jsnack-3/bonus/script.js
+++ jsnack-3/bonus/script.js
@@ -3,10 +3,18 @@
 var somma=0;
 //CICLO FOR
 for (var i = 0; i < 5; i++) {
+  //chiedo di inserire un numero
   var nums = parseInt(prompt("Inserisci un numero"));
-  console.log(nums);
-  somma = somma + nums;
-  //OPPURE somma += nums
+  //condizione all'interno del ciclo per obbligare ad inserire un numero
+  if(isNaN(nums)){
+    alert("Ti ho detto NUMERO");
+    //condizione all'interno dell'if per far si che la mia i(index) retroceda di una posizione se il valore inserito non è un numero
+    i--;
+  } else{
+    console.log(nums);
+    somma = somma + nums;
+    //OPPURE somma += nums
+  }
 }
 document.getElementById('somma').innerHTML = somma;
 console.log(somma);
@@ -16,6 +24,15 @@ var j = 0;
 while(j<5){
   var nums = parseInt(prompt("Inserisci un numero"));
   console.log(nums)
+  if (isNaN(nums)){
+    alert("Ti ho detto NUMERO");
+    //condizione all'interno dell'if per far si che la mia j(index) retroceda di una posizione se il valore inserito non è un numero
+    j--;
+  }else{
+    console.log(nums);
+    somma = somma + nums;
+    //OPPURE somma += nums
+  }
   somma = somma + nums;
   j++;
 }
