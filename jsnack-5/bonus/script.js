673cd1d9de90bdb3891342c6a3400fcf1018b153
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-27 19:42:13 +0100

    fix layout for jsnack5

--- jsnack-5/bonus/script.js
+++ jsnack-5/bonus/script.js
@@ -1,14 +1,30 @@
 
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 // un numero, se è dispari inseriscilo nell’array.
-//@ts-check
-
+var global = [];
 var array = [];
+var number;
+
+
+
 for(var i=0; i < 6 ; i++){
-  var number = parseInt(prompt("inserisci un numero"));
-  if(number % 2 !== 0){
-    array.push(number);
+  number = parseInt(prompt("inserisci un numero"));
+  if(isNaN(number)){
+    alert("Ti ho detto NUMERO");
+    i--;
+  }else{
+    if (number % 2 !== 0) {
+      array.push(number);
+      global.push(number)
+    }else{
+      global.push(number);
+    } 
   }
-}
-document.getElementById("odd").innerHTML = "I numeri dispari da te inseriti sono :" + array;
-console.log(array);
+ }
+
+  
+
+document.getElementById("globalNumbers").innerHTML = "I numeri da te inseriti sono :"
+document.getElementById("numbers").innerHTML = global;
+document.getElementById("globalOdd").innerHTML = "I numeri <span>dispari</span> da te inseriti sono :" 
+document.getElementById("odd").innerHTML = array;
